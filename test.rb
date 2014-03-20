64971dac759fe70e7664f997d8a5c10159a4b42d
emilssonn <pe222bz@student.lnu.se>
2014-03-20 23:05:00 +0100

    Removed unused test files

--- test.rb
+++
@@ -1,8 +0,0 @@
-require 'oauth2'
-
-callback = "urn:ietf:wg:oauth:2.0:oob"
-app_id = "c7c93e084895a2b37c75f92a1691e4a91a9371d24b721ec737a914019cf802fd"
-secret = "0bb29c4e1e48761e918640831f9499ceac44db552881f4a53fb5918699989f0f"
-client = OAuth2::Client.new(app_id, secret, site: "http://lvh.me:3000/")
-puts client.auth_code.authorize_url(redirect_uri: callback)
-
--- test2.rb
+++
@@ -1,7 +0,0 @@
-require 'oauth2'
-callback = "urn:ietf:wg:oauth:2.0:oob"
-app_id = "c7c93e084895a2b37c75f92a1691e4a91a9371d24b721ec737a914019cf802fd"
-secret = "0bb29c4e1e48761e918640831f9499ceac44db552881f4a53fb5918699989f0f"
-client = OAuth2::Client.new(app_id, secret, site: "http://lvh.me:3000/")
-access = client.auth_code.get_token('6b51db7551d1964322f9c93fd28e8a109640b72ff0d03b1f80a3d0f5d1a4fd8f', redirect_uri: callback)
-puts access.token
