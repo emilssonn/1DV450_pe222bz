aca1dd4fc2825ee7524b6a0a1f4ca3418dcc247a
emilssonn <pe222bz@student.lnu.se>
2014-04-11 11:58:44 +0200

    Made page filter case insensitive

--- TOERHClient/app/assets/javascripts/filters/sorter.js
+++ TOERHClient/app/assets/javascripts/filters/sorter.js
@@ -9,13 +9,14 @@ angular.module('TOERH.filters').filter('sorter', function () {
         }
 
         searchProp = angular.lowercase(searchProp);
-
+        string = angular.lowercase(string);
         //Filter the list to only show matching objects
         //If the value being targeted for filtering is a array, check for match on each value in array
         return list.filter(function (value) {
             value = angular.lowercase(value[searchProp]);
             if (angular.isArray(value)) {
                 return value.filter(function (val) {
+                    val = angular.lowercase(val);
                     return ~val.indexOf(string);
                 }).length > 0;
             }
